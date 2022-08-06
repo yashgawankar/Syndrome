6f2e076edfc85d139beddd7809c75272112321b5
Yash Gawankar <yash.gawankar@bankbuddy.ai>
2022-08-07 01:55:42 +0530

    Basic LSTM code for forecasting

--- modules/main.py
+++ modules/main.py
@@ -4,7 +4,56 @@ import pandas as pd
 df = pd.DataFrame()
 path = '../data/'
 for file in os.listdir(path):
-    df_ = pd.read_csv(path + file,index_col=0)
+    df_ = pd.read_csv(path + file)
     df = pd.concat([df,df_])
 
-print(df.shape)
+# print(df.shape)
+
+# univariate lstm example
+from numpy import array
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+
+
+df = df[(df['Supplier Name']=='Globant') & (df['Function']=='Information Technology And Services') &
+        (df['Service']=='Email Services') & (df['Country']=='New Zealand')]
+# print(df.columns)
+
+# split a univariate sequence into samples
+def split_sequence(sequence, n_steps):
+	X, y = list(), list()
+	for i in range(len(sequence)):
+		# find the end of this pattern
+		end_ix = i + n_steps
+		# check if we are beyond the sequence
+		if end_ix > len(sequence)-1:
+			break
+		# gather input and output parts of the pattern
+		seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+		X.append(seq_x)
+		y.append(seq_y)
+	return array(X), array(y)
+
+# define input sequence
+# raw_seq = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+raw_seq = list(df['Resources'].values)
+# choose a number of time steps
+n_steps = 3
+# split into samples
+X, y = split_sequence(raw_seq, n_steps)
+# reshape from [samples, timesteps] into [samples, timesteps, features]
+n_features = 1
+X = X.reshape((X.shape[0], X.shape[1], n_features))
+# define model
+model = Sequential()
+model.add(LSTM(50, activation='relu', input_shape=(n_steps, n_features)))
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mse')
+# fit model
+model.fit(X, y, epochs=50, verbose=1)
+# demonstrate prediction
+x_input = array([70, 80, 90])
+x_input = x_input.reshape((1, n_steps, n_features))
+yhat = model.predict(x_input, verbose=0)
+print(yhat)
